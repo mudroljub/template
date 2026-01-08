5ed804980887b0840ff9297f948e77ce7f38b079
Damjan Pavlica <mudroljub@gmail.com>
2026-01-08 16:20:20 +0100

    update layout

--- src/components/Template/Template2.tsx
+++ src/components/Template/Template2.tsx
@@ -14,20 +14,22 @@ export default function Template2({ expiredIn, bgImage }: Template2Props) {
             className={classnames(styles.container, styles.container2)}
             style={bgImage ? { backgroundImage: `url(${bgImage})` } : undefined}
         >
-            <div className={styles.item}>
-                <div>
-                    <h1 className={styles.title}>Get new ships for secret manoeuvres battle type!</h1>
-                    {expiredIn && <Timer expiredIn={expiredIn} />}
+            <div className={styles.bottomWrap}>
+                <div className={styles.column}>
+                    <div>
+                        <h1 className={styles.title}>Get new ships for secret manoeuvres battle type!</h1>
+                        {expiredIn && <Timer expiredIn={expiredIn} />}
+                    </div>
                 </div>
-            </div>
-            <div className={styles.item}>
-                <div>
-                    <p><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam tenetur nesciunt maiores, sed ipsam molestias, ducimus tempora magni, ipsum doloribus maxime laboriosam eos ratione! Sit quis repellendus cumque enim eligendi.</span><span>Maxime perferendis esse dolor accusantium quod sint molestias placeat ipsum laboriosam, deleniti maiores dolores magni? Qui optio eveniet sit blanditiis vel. Officiis cupiditate dolorum delectus ullam quidem commodi quod quas.</span></p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto eaque sed provident, veritatis inventore iste rerum blanditiis aliquid sint culpa, totam esse sit dignissimos deleniti quaerat itaque tempore odio mollitia?</p>
-                </div>
-                <div>
-                    <button>Get Started</button>
-                    <button>Read More</button>
+                <div className={styles.column}>
+                    <div>
+                        <p><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam tenetur nesciunt maiores, sed ipsam molestias, ducimus tempora magni, ipsum doloribus maxime laboriosam eos ratione! Sit quis repellendus cumque enim eligendi.</span><span>Maxime perferendis esse dolor accusantium quod sint molestias placeat ipsum laboriosam, deleniti maiores dolores magni? Qui optio eveniet sit blanditiis vel. Officiis cupiditate dolorum delectus ullam quidem commodi quod quas.</span></p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto eaque sed provident, veritatis inventore iste rerum blanditiis aliquid sint culpa, totam esse sit dignissimos deleniti quaerat itaque tempore odio mollitia?</p>
+                    </div>
+                    <div>
+                        <button>Get Started</button>
+                        <button>Read More</button>
+                    </div>
                 </div>
             </div>
         </div>
