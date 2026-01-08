da5c3d5910dad802b0d2fec5db156a1c49e07b67
Damjan Pavlica <mudroljub@gmail.com>
2026-01-08 16:42:21 +0100

    Template2.module.css

--- src/components/Template/Template2.tsx
+++ src/components/Template/Template2.tsx
@@ -1,6 +1,7 @@
 import classnames from 'classnames';
 
 import shared from './Template.module.css'
+import styles from './Template2.module.css'
 import Timer from '../Timer/Timer'
 
 interface Template2Props {
@@ -11,17 +12,17 @@ interface Template2Props {
 export default function Template2({ expiredIn, bgImage }: Template2Props) {
     return (
         <div
-            className={classnames(shared.container, shared.container2)}
+            className={classnames(shared.container, styles.container2)}
             style={bgImage ? { backgroundImage: `url(${bgImage})` } : undefined}
         >
-            <div className={shared.bottomWrap}>
-                <div className={shared.column}>
+            <div className={styles.bottomWrap}>
+                <div className={styles.column}>
                     <div>
                         <h1 className={shared.title}>Get new ships for secret manoeuvres battle type!</h1>
                         {expiredIn && <Timer expiredIn={expiredIn} />}
                     </div>
                 </div>
-                <div className={shared.column}>
+                <div className={styles.column}>
                     <div>
                         <p><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam tenetur nesciunt maiores, sed ipsam molestias, ducimus tempora magni, ipsum doloribus maxime laboriosam eos ratione! Sit quis repellendus cumque enim eligendi.</span><span>Maxime perferendis esse dolor accusantium quod sint molestias placeat ipsum laboriosam, deleniti maiores dolores magni? Qui optio eveniet sit blanditiis vel. Officiis cupiditate dolorum delectus ullam quidem commodi quod quas.</span></p>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto eaque sed provident, veritatis inventore iste rerum blanditiis aliquid sint culpa, totam esse sit dignissimos deleniti quaerat itaque tempore odio mollitia?</p>
